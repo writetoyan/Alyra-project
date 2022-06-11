747faf326d5ae9e62982d0293c6db4370dc334a8
writetoyan <writetoyan@protonmail.com>
2022-06-11 16:22:17 +0200

    version1

---
+++ voting.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract Voting is Ownable{
+
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event ProposalRegistered(uint ProposalId);
+    event Voted (address voter, uint proposalId);
+
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalRegistrationStarted,
+        ProposalRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    WorkflowStatus public workflowStatus;
+    mapping(address => Voter) whitelist;
+    Proposal[] public proposals;
+    uint proposalId = 0;
+    uint public winningProposalId; 
+
+    function nextStatus() public onlyOwner {
+        require(uint(workflowStatus) < 6, "Congratulation, your voting session was a success!");
+        WorkflowStatus previousStatus = workflowStatus;
+        workflowStatus = WorkflowStatus (uint(workflowStatus) + 1);
+        emit WorkflowStatusChange(previousStatus, workflowStatus);
+    }
+
+    function register(address _voterAddress) public onlyOwner {
+        require(workflowStatus == WorkflowStatus.RegisteringVoters, "This is not registration period!");
+        whitelist[_voterAddress].isRegistered = true;
+        emit VoterRegistered(_voterAddress);
+    }
+
+    function makeProposal(string calldata _description) public {
+        require(workflowStatus == WorkflowStatus.ProposalRegistrationStarted, "This is not proposition period!");
+        require(whitelist[msg.sender].isRegistered, "You have to be registered in order to make proposal");
+        Proposal memory proposal = Proposal(_description, 0);
+        proposals.push(proposal);
+        emit ProposalRegistered(proposalId);
+        proposalId++;
+    }
+
+    function vote(uint _proposalId) public {
+        require(workflowStatus == WorkflowStatus.VotingSessionStarted, "This is not voting period!");
+        require(whitelist[msg.sender].isRegistered, "You are not registered!");
+        require(!whitelist[msg.sender].hasVoted, "You cannot vote two times!");
+        require(_proposalId < proposals.length, "This proposal do not exist!");
+        whitelist[msg.sender].hasVoted = true;
+        whitelist[msg.sender].votedProposalId = _proposalId;
+        proposals[_proposalId].voteCount += 1;
+        emit Voted(msg.sender, _proposalId);
+    } 
+
+    function countVote() public onlyOwner returns (uint _winningProposalId) {
+        require(workflowStatus == WorkflowStatus.VotingSessionEnded, "This is not counting period!");
+        uint mostVoted = 0;
+        for (uint i = 0; i < proposals.length; i++) {
+            if (proposals[i].voteCount > mostVoted) {
+                mostVoted = proposals[i].voteCount;
+               _winningProposalId = i;
+            }
+        } 
+        winningProposalId = _winningProposalId;
+    }
+
+    function checkWinnerDetails() public view returns (string memory _description, uint _voteCount) {
+        require(workflowStatus == WorkflowStatus.VotesTallied, "Winning proposal will be published soon");
+        _description = proposals[winningProposalId].description;
+        _voteCount = proposals[winningProposalId].voteCount;
+    }
+
+}
